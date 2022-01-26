873341c360100cdf0b4d6053cef6ab7e503af428
Abfahim21 <abrarfahim21@gmail.com>
2022-01-27 04:09:04 +0600

    number methods

---
+++ numberMethods.js
@@ -0,0 +1,16 @@
+//toString() method returns a number as a string.
+let a = 23;
+let b = a.toString();
+console.log(typeof(b));
+
+//toExponential() returns a string,
+//with a number rounded and written using exponential notation.
+let x = 9.66565656;
+let y = x.toExponential(1);
+console.log(y);
+
+//toFixed() returns a string, with the number written with a specified number of decimals
+//toFixed(2) is perfect for working with money.
+let p = 5.987654;
+let q = p.toFixed(2);
+console.log(q);
