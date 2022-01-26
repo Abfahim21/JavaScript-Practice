7503854629c2898ab7ed3357f2c5aa9c8e4e3602
Abfahim21 <abrarfahim21@gmail.com>
2022-01-26 22:55:50 +0600

    array

---
+++ array.js
@@ -0,0 +1,12 @@
+const carName = ['BMW', 'Audi', 'Mercedez Benz', 'Porsche'];
+let firstBrandName = carName[0];
+console.log(carName);
+console.log(firstBrandName);
+console.log(carName1);
+
+const person = {
+    firstName:"John", lastName:"Doe", age:46
+};
+console.log(person.firstName);
+console.log(person.lastName);
+console.log(person.age);
