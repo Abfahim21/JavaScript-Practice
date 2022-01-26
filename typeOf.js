fe05fa68752557dfba9c6e77d50e12bf31eac402
Abfahim21 <abrarfahim21@gmail.com>
2022-01-26 23:01:07 +0600

    typeOf

---
+++ typeOf.js
@@ -0,0 +1,24 @@
+let a = "Hello";
+let b = 34;
+let c= '';
+let d;
+let e = null;
+let f = NaN;
+
+let a1 = typeof(a);
+console.log(a1);
+
+let b1 = typeof(b);
+console.log(b1);
+
+let c1 = typeof(a);
+console.log(a1);
+//empty = indefined
+let d1 = typeof(d);
+console.log(d1);
+//null is a object
+let e1 = typeof(e);
+console.log(e1);
+//Nan is a Number
+let f1 = typeof(f);
+console.log(f1);
